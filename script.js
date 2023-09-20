4e6c2a745f4983909e9fc472ed80a8edb05c0f64
JaimeCs27 <jaimecab2002@gmail.com>
2023-09-20 17:50:32 -0600

    prueba boton

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btn = document.querySelector('.btn');
+const loginForm = document.getElementById('loginForm');
 
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let username = document.getElementById('username');
+    let pass = document.getElementById('pass');
+    console.log(username.value);
+
+})
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -12,8 +21,5 @@ loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 })
 
-btn.addEventListener('click', () => {
-    console.log('Hola');
-})
 
 
